a43908f775a837c6aaf7ea39885997ac0f1d0332
xinyu-hou <xinyu-hou@qq.com>
2026-02-21 12:04:08 +0800

    fix(用户管理):之前生成器生成的代码git未追踪，补充提交

---
+++ src/main/java/com/miao/ai_gen_web/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.miao.ai_gen_web.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.miao.ai_gen_web.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author miao
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
